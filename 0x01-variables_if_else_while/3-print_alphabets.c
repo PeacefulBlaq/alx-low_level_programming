88bb1761eb2ef862374345d72c860ec8e66727be
PeacefulBlaq <abidemibakre@gmail.com>
2023-07-22 06:34:11 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** This function prints alphabets a to z
+ * both in lowercase all through
+ * and in upper case all through
+ */
+
+int main(void)
+{
+	char lowercase_letter, uppercase_letter;
+
+	printf("Lowercase alphabets: ");
+	
+	for(lowercase_letter = 'a'; lowercase_letter <= 'z'; ++lowercase_letter){
+		printf("%c ", lowercase_letter);
+	}
+
+	for(uppercase_letter = 'Z'; uppercase_letter <= 'Z'; ++uppercase_letter){
+		printf("%c", uppercase_letter);
+	}
+
+return (0);
+}
