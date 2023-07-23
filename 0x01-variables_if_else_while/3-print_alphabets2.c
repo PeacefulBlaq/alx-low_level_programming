ad1ce386d8379e0a4378cb680ac68d9d0e417e24
PeacefulBlaq <abidemibakre@gmail.com>
2023-07-23 08:14:12 -0700

    Updated

--- 0x01-variables_if_else_while/3-print_alphabets2.c
+++ 0x01-variables_if_else_while/3-print_alphabets2.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char  i;
 
-	for(i = 'A'; i <= 'Z'; ++i);
+	for (i = 'A'; i <= 'Z'; ++i);
 	printf("%c", i);
 
-	for(i = 'a'; i <='z'; ++i);
+	for (i = 'a'; i <='z'; ++i);
 	printf("%c", i);
 
 	return (0);
