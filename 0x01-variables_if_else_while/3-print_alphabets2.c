1b2651c3b2fc2f4ecec782253cf0195d32f0416f
PeacefulBlaq <abidemibakre@gmail.com>
2023-07-23 07:37:24 -0700

    Printing alphabets both in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * C programme to print all alphabets in both
+ * upper and lower case
+ * */
+
+int main()
+{
+	char  i;
+
+	for(i='A'; i<= 'Z'; ++i);
+	printf("%c",i);
+
+	for(i='a'; i<='z'; ++i);
+	printf("%c",i);
+
+	return 0;
+}
