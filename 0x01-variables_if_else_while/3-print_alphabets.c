d3b153c1e51443b55a535e47851cecca6a2f716b
PeacefulBlaq <abidemibakre@gmail.com>
2023-07-23 10:50:20 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
-
 /**
- * C programme to print all alphabets in both
- * upper and lower case
- * */
+ * C programme to print all alphabets in both upper and lower case even though
+ * that is all about this my function, I will write
+ * more since this is what Betty wants
+ */
 
-int main()
+int main(void)
 {
 	char  i;
 
-	for(i='A'; i<= 'Z'; ++i);
-	printf("%c",i);
+	for (i = 'A'; i <= 'Z'; ++i)
+	{
+		printf("%c", i);
+	}
 
-	for(i='a'; i<='z'; ++i);
-	printf("%c",i);
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		printf("%c", i);
+	}
 
-	return 0;
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** C function to print alphabets in lower case with a new line */
+
+char i;
+
+int main(void)
+{
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		printf("%c\n", i);
+	}
+
+Return: (0);
+}
